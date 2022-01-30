948eae4afa28f7cad9210b1e8ef8b6c37a84581c
Kike Esteban <autokike@gmail.com>
2022-01-30 11:55:13 +0100

    Ejercicio 16

---
+++ ejercicio_16/index.js
@@ -0,0 +1,9 @@
+const links = document.getElementsByTagName('a');
+const tercerParrafo = document.getElementById("third-paragraph")
+const linksTercerParrafo = tercerParrafo.getElementsByTagName('a')
+
+var newDiv = document.createElement("div");
+newDiv.innerHTML = `Número total de enlaces: ${links.length}.<br/>
+    Link del penúltimo enlace: ${links[links.length-2].href}.<br/>
+    Número de enlaces del tercer párrafo: ${linksTercerParrafo.length}.`
+document.body.appendChild(newDiv);
